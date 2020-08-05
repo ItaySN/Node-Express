4e11a20be5229506f2284ce6a8a159e30bbabca1
ItaySN <itayfic@gmail.com>
2020-08-05 17:28:27 +0300

    Create the get,post,delete,put(update) func with an app listen on 3001 port.

--- rest.js
+++ rest.js
@@ -15,3 +15,51 @@ let products = [
         content: 'Very bad post'
     }
 ];
+
+
+app.post('/product', (req, res) =>{
+    console.log(req.body);
+    products.push(req.body);
+    res.send(req.body);
+    
+});
+
+
+app.delete('/product/:id', (req, res) =>{
+    products.forEach((product, index) =>{
+        if(product.id === req.params.id){
+            products.splice(index, 1);
+            res.send(req.params.id + ' deleted');
+        }
+    });
+});
+
+
+app.put('/product/:id', (req, res) =>{
+    products.forEach((product, index) =>{
+        if(product.id === req.params.id){
+            products[index] = req.body;
+            res.send(req.body);
+        }
+    });
+});
+
+
+app.get('/product/:id', (req, res) =>{
+    for(let product of products){
+        if(product.id === req.params.id){
+            res.send(post);
+        }
+    }
+});
+
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/', (req, res) => {
+    res.send('Shopping List ');
+});
+
+
+app.listen(3001);
